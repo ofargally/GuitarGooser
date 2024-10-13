1eea428cb7d3b4c0022526cc40e51e7a7c673eb4
ofargally <ofargally@colgate.edu>
2024-10-13 17:36:24 +0200

    modified sprites and added animation

---
+++ Assets/Scripts/playerAnimate.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+public class playerAnimate : MonoBehaviour
+{
+
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    public Sprite[] playerIdleSprites;
+    public Sprite[] HitUpSprites;
+    public Sprite[] HitDownSprites;
+    public Sprite[] HitLeftSprites;
+    public Sprite[] HitRightSprites;
+
+    public Sprite[] GettingDamageSprites;
+    public float animationSpeed = 0.2f;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void PlayerHit()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            StartCoroutine(LoopThroughSprites(HitLeftSprites));
+        }
+    }
+    IEnumerator LoopThroughSprites(Sprite[] sprites)
+    {
+        int index = 0;
+        while (sprites != null && sprites.Length > 0)
+        {
+            spriteRenderer.sprite = sprites[index];
+            index = (index + 1) % sprites.Length;
+            yield return new WaitForSeconds(animationSpeed);
+        }
+    }
+}
